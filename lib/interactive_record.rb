9a5be51c447f79068a30c933a55e392baba9f56f
Omar Nabi <onabi20@gmail.com>
2020-05-31 18:11:30 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -52,8 +52,8 @@ class InteractiveRecord
   end
   
   def self.find_by_name(name)
-  sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
-  DB[:conn].execute(sql, name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
+    DB[:conn].execute(sql, name)
   end
 
 end
