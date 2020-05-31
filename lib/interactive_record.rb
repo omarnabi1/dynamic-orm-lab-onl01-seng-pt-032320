3a4fa7302b3662e0cc15fa628b2a719b8cf73361
Omar Nabi <onabi20@gmail.com>
2020-05-31 17:32:42 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -33,7 +33,8 @@ class InteractiveRecord
   end
   
   def col_names_for_insert
-    self.class.column_names.delete_if {|col| col == "id"}.join
+    self.class.column_names.delete_if {|col| col == "id"}.join(",")
+  end
 
   
   
